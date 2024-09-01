50859a789717aeac93237cae5458cd55de663202
user <a_n_t_o_n99@mail.ru>
2024-09-02 01:28:19 +0300

    Complited

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+        String firstName = "Ivanov";
+        String middleName = "Ivan";
+        String lastName = "Ivanovich";
+        String fullName = firstName + ' ' + middleName + ' ' + lastName;
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета —" + fullName.toUpperCase());
+        fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё", "е");
+        System.out.println("Данные Ф. И. О. сотрудника — " + fullName);
+
 
     }
 }
